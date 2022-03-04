0ebe46212ebdbeeb44215210302d2ef433bf3463
larissa-oliv <lah.rio123@gmail.com>
2022-03-04 15:02:07 -0300

    CRUD completo de Tema

--- src/app/delete/tema-delete/tema-delete.component.ts
+++ src/app/delete/tema-delete/tema-delete.component.ts
@@ -12,6 +12,7 @@ import { environment } from 'src/environments/environment.prod';
 export class TemaDeleteComponent implements OnInit {
 
   tema: Tema = new Tema()
+  idTema: number
 
   constructor(
     private temaService: TemaService,
@@ -23,6 +24,22 @@ export class TemaDeleteComponent implements OnInit {
     if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
+
+    this.idTema = this.route.snapshot.params['id']
+    this.findByIdTema(this.idTema)
+  }
+
+  findByIdTema(id:number){
+    this.temaService.getByIdTema(id).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+  }
+
+  apagar(){
+    this.temaService.deleteTema(this.idTema).subscribe(()=>{
+      alert('Tema apagado com sucesso!')
+      this.router.navigate(['/tema'])
+    })
   }
 
 }
